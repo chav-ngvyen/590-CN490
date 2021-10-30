05dba6901c579565f06a5bea174fdbe100375b57
Chau Nguyen <cn490@georgetown.edu>
2021-10-29 22:18:11 -0400

    More cleaning up

--- HW5.0/RUN.sh
+++ HW5.0/RUN.sh
@@ -1,20 +1,9 @@
 ScriptLoc=${PWD}
-# LogLoc=${PWD/Logs}
 
-# cd $ScriptLoc
-# # for i in *.py 
-# for i in 02-train.py
-# do 
-# 	echo "-----------" $i "-----------" 
-# 	python $i > cd $LogLoc > $i.txt #run all python scripts 
-# 	grep "I HAVE WORKED"
-# done
-# exit
 cd $ScriptLoc
 for i in 0*.py
 do
 	filename=${i%%.*}
-	# echo $filename$_log.txt
 	echo "---Running---" $i "-----------"
 	python $i | tee $ScriptLoc/Logs/$filename$_log.txt
 	echo "---Done with---" $i 
